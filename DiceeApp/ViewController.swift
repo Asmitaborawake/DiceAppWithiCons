541376301332d2e230d26924189ed7f23dab11cf
Asmita <asmitaborawake@gmail.com>
2019-03-26 13:16:08 +0530

    push

--- DiceeApp/ViewController.swift
+++ DiceeApp/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var randomDeicIndexOne : Int = 0
+    var randomDiceIndexTwo : Int = 0
+    var diceImage = ["dice1","dice2","dice3","dice4","dice5","dice6"]
+    @IBOutlet weak var diceOne: UIImageView!
+    @IBOutlet weak var diceTwo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        diceAction()
     }
 
-
+    @IBAction func rollBTNClick(_ sender: Any) {
+       diceAction()
+    }
+    
+    func diceAction() {
+        randomDeicIndexOne = Int.random(in: 0 ... 5)
+        randomDiceIndexTwo = Int.random(in: 0 ... 5)
+        
+        diceOne.image = UIImage(named: diceImage[randomDeicIndexOne])
+        diceTwo.image = UIImage(named: diceImage[randomDiceIndexTwo])
+        
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        diceAction()
+    }
+    
+    
+    
 }
 
